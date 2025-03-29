0dfd88749416d232a1711141b46a9dcfe486b338
framirezj <12649259+framirezj@users.noreply.github.com>
2025-03-29 11:59:16 -0300

    created: proyecto pizzeria y los primeros componentes

---
+++ pizzeria_1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Navbar, Home, Footer } from './components'
+ 
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
