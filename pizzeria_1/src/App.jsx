ba52d578489d5eb3fbc365c3c09feb1c977cc506
framirezj <12649259+framirezj@users.noreply.github.com>
2025-03-29 22:39:02 -0300

    added: contenido al componente navbar

--- pizzeria_1/src/App.jsx
+++ pizzeria_1/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
--- pizzeria_1/src/components/Navbar.jsx
+++ pizzeria_1/src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import Button from "react-bootstrap/Button";
+import "../../src/index.css";
+
 const Navbar = () => {
+  return (
+    <div className="contenedor-navbar">
+      <h2>Pizzería Mamma Mia!</h2>
+      <div className="nav-buttons">
+        <Button variant="outline-light">🍕 Home</Button>
+        <Button variant="outline-light">🔐 Login</Button>
+        <Button variant="outline-light">🔐 Register</Button>
+      </div>
+      <Button variant="outline-info" className="total-button">
+        🛒 Total: $
+      </Button>
+    </div>
+  );
+};
 
-}
+export default Navbar;
 
-export default Navbar
+/*
+a. 🍕 Home
+b. 🔓 Profile
+c. 🔒 Logout
+d. 🔐 Login
+e. 🔐 Register
+f. 🛒 Total: $
+*/
