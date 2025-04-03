418a3ec40d718cb2d6330508fe41d9d42f75a416
framirezj <12649259+framirezj@users.noreply.github.com>
2025-04-03 12:35:10 -0300

    added tips para el deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,19 +13,19 @@ const Home = () => {
           name="Napolitana"
           price={5950}
           ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-          img="/img/pizzapizza.webp"
+          img="./img/pizzapizza.webp"
         />
         <CardPizza
           name="Española"
           price={6950}
           ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-          img="/img/pizzapizza.webp"
+          img="./img/pizzapizza.webp"
         />
         <CardPizza
           name="Pepperoni"
           price={6950}
           ingredients={["mozzarella", "pepperoni", "orégano"]}
-          img="/img/pizzapizza.webp"
+          img="./img/pizzapizza.webp"
         />
       </div>
     </>
