edfeaff056b84c641e2aeea04587b234ccbc277d
framirezj <12649259+framirezj@users.noreply.github.com>
2025-04-08 21:10:59 -0400

    added: configuracion de estados para los input del componente

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
+
 const Register = () => {
+
+    //estados
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
   return (
     <>
       <form style={{color: "black"}}>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="email" value={email} />
         </div>
         <div>
           <label htmlFor="password">Contraseña:</label>
-          <input type="password" id="password" name="password" />
+          <input type="password" id="password" name="password" value={password}/>
         </div>
 
         <div>
           <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
-          <input type="password" id="confirmPassword" name="confirmPassword" />
+          <input type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword}/>
         </div>
 
         <button type="submit">Registrarse</button>
