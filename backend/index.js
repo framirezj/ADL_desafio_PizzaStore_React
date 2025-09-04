19d2c3445b099b00a9bc53604a2a07b8937220d9
framirezj <12649259+framirezj@users.noreply.github.com>
2025-09-04 16:43:01 -0400

    update cors para render

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,28 @@ import pizzaRoute from "./routes/pizza.route.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+// Configuración de CORS para permitir tu frontend específico
+const allowedOrigins = [
+  'https://adl-desafio-pizzastore-react.onrender.com'
+];
+
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir requests sin 'origin' (como Postman, móviles, etc.)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      // Si el origen está en la lista blanca
+      callback(null, true);
+    } else {
+      // Si el origen no está permitido
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Si necesitas enviar cookies o autenticación
+}));
 
 app.use("/api/auth", authRoute);
 app.use("/api/pizzas", pizzaRoute);
